f25cdd046ab217f2d7316aa787432ee1e162c9e3
sumaiyachy <96937337+sumaiyachy@users.noreply.github.com>
2022-10-08 16:09:15 +0600

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-5xl">
+      <h1>Hello service...</h1>
+      <h1>I am service...</h1>
+      <h1>It is  gghssdsdsd</h1>
+      <h1>Hello service...</h1>
+      <h5>I am very tired </h5>
+      <p>rfg Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, voluptatum.</p>
+       
     </div>
   );
 }
